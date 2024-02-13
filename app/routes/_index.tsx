b960bbd830ff3f10aa3835eadd855533c0b84e8d
Joaquin Bravo Contreras <jackbravo@gmail.com>
2024-02-13 08:27:54 -0600

    Add MUI and Material Core and use them

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,27 +13,10 @@ export default function Index() {
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <a href="./simple-table">Simple Table</a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <a href="./material-table">Material Table</a>
         </li>
       </ul>
     </div>
